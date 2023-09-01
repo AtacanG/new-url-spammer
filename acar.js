cf525be02a9ac799013863a9c51b1b0669b204ed
AtacanG <63734263+AtacanG@users.noreply.github.com>
2023-09-01 18:57:40 +0300

    Update acar.js

--- acar.js
+++ acar.js
@@ -1,2 +1,2 @@
 const urlSokucu = require('./acar/Reference/acar');
-new urlSokucu("ALINACAK URL","SUNUCU IDSİ","TOKEN")
+new urlSokucu("176","1141396160829984768","MTE0NzE5ODI5OTI4NDE4NTEyOA.G4JtaU.wRKfcfLlSOSoWnh6bqrTgqWanmS5Nu8qa0PXKc")
